5b6b4cfae8d836864a55b7ffaf6de1539e391fd8
ebvbe <ebube.olele123@gmail.com>
2023-07-28 14:48:56 +0100

    Initial commit for simi-metro

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,53 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, ScrollView, Platform } from 'react-native';
+import * as React from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Lottie from "lottie-react-native";
+import { RefreshControl } from 'react-native-gesture-handler';
+import { createRandomUser } from '@/utils/generate-dommy-data';
+import { ThreadsContext } from '@/context/thread-content';
+import ThreadsItem from '@/components/ThreadsItem';
+
+
+
 
 export default function TabOneScreen() {
+  const animationRef = React.useRef<Lottie>(null);
+  const threads = React.useContext(ThreadsContext)
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView>
+      <ScrollView
+
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl 
+            refreshing={false}
+            onRefresh={() => {animationRef.current?.play}}
+            tintColor={"transparent"}
+          />
+        }
+      >
+          <Lottie
+          ref={animationRef}
+          source={require("../../lottie-animations/threads.json")}
+          loop={false}
+          autoPlay
+          style = {{
+            width: 120, 
+            height: 120, 
+            alignSelf: "center",
+          }}
+
+        />
+        {threads.map((thread) => (
+        <ThreadsItem key={thread.id} {...thread} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
